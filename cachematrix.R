f74a7da5443c3280c0094c2730f9054da13a06af
aislanzap <41380019+aislanzap@users.noreply.github.com>
2018-08-16 15:05:28 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Code written in order to Coursera 2nd Programming Practice
+## The idea is save time of proccessing getting cached data
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## The makeCacheMatrix function creates a list with four functions:
+## function set: set the value of matrix
+## function get: get the value of matrix
+## function set: setinv the value of matrix
+## function get: getinv the value of matrix
 
+makeCacheMatrix<- function(x=matrix()){
+          #start a Null variable          
+          inv<-NULL
+          
+          #set the matrix
+          set <-function(y){            
+                    x<<-y 
+                    inv<<- NULL
+          }
+          
+          #get the matrix
+          get<-function() x
+          
+          #set the inverse of matrix
+          setinv<- function(inverse) inv <<- inverse
+          
+          #get the inverse of matrix
+          getinv <- function() inv
+          list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## The cacheSolve function calcule the inverse matrix, 
+##but before it checks if it has already been inverted to get the result from cache
+cacheSolve<-function(x,...){
+          inv<- x$getinv()    #bring the inverse to the inv variable
+          
+          # check if the inverse has already been calculated
+          if(!is.null(inv)){
+                    message("Getting cached data")
+                    return(inv)
+          }
+          #solve the inverse in case of the inverse is NULL
+          data<- x$get()
+          inv<- solve(data)
+          x$setinv(inv)
+          inv
 }
